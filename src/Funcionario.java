b2a080f4d4eb3ebd8f740a8c3fcff8c8ce0502bd
lucac <lucacallegari71@gmail.com>
2025-04-27 02:49:22 -0300

    Adicionando o sitema básico para funcionários

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,29 +1,52 @@
 public class Funcionario {
-    private int matricula;
+    private int id;
     private String nome;
     private String email;
 
-    public int getMatricula() {
-        return matricula;
+    //Entidade Funcionário
+    public Funcionario(int id, String nome, String email){
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+    
+    //Pedir o ID de um funcionário
+    public int getId(){
+        return id;
     }
 
-    public void setMatricula(int matricula) {
-        this.matricula = matricula;
-    }
-
-    public String getNome() {
+    //Pedir/Mudar o Nome de um funcionário
+    public String getNome(){
         return nome;
     }
 
-    public void setNome(String nome) {
+    public void setNome(String nome){
         this.nome = nome;
     }
 
+    //Pedir/Mudar o email de um Funcionário
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
+    public void setEmail(String email){
         this.email = email;
+    }
+
+    //Criar um novo Funcionário
+    @override
+    public String toString() {
+        return "ID do Funcionario: " + id + "\nNome do Funcionario: " + nome + "\n Email do Funcionario: " + email;
+    }
+
+    //Transformar em arquivo txt
+    public String toCSV() {
+        return id + "," + nome + "," + email;
+    }
+
+    //Pegar as informações do arquivo txt
+    public static Funcionario fromCSV (String linha){
+        String[] partes = linha.split(",");
+        return new Funcionario(Integer.parseInt(partes[0], partes[1], partes[2]));
     }
 }
